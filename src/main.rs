bfd42ca81de9f89bb5f7c7a26c6711818465ce0d
Alan Fayz <alan.fayz@hotmail.com>
2026-03-12 00:46:28 +0000

    db scan with images

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::{collections::HashMap, 
-    time::{SystemTime, UNIX_EPOCH}}
-;
+use std::{
+    collections::HashMap,
+    time::{Instant, SystemTime, UNIX_EPOCH},
+};
 
 use macroquad::prelude::*;
 
-use crate::{dbscan::{Sample, scan}, spatial_hash::Spatial};
+use crate::{
+    dbscan::{Sample, scan},
+    spatial_hash::Spatial,
+};
 
 mod dbscan;
 mod spatial_hash;
@@ -14,9 +18,28 @@ struct SamplePoint {
     pub position: Vec2,
 }
 
+#[derive(Default, Debug, Copy, Clone)]
+struct ImagePoint {
+    pub position: Vec2,
+    pub color: Color,
+}
+
 impl Sample for SamplePoint {
     fn distance_squared(self: &Self, right: &Self) -> f32 {
         self.position.distance_squared(right.position)
+    }
+}
+
+impl Sample for ImagePoint {
+    fn distance_squared(&self, right: &Self) -> f32 {
+        let dx = self.position.x - right.position.x;
+        let dy = self.position.y - right.position.y;
+
+        let dr = (self.color.r - right.color.r) * (screen_width());
+        let dg = (self.color.g - right.color.g) * (screen_height());
+        let db = (self.color.b - right.color.b) * (screen_width());
+
+        return (dx * dx) + (dy * dy) + (dr * dr) + (dg * dg) + (db * db);
     }
 }
 
@@ -24,6 +47,35 @@ impl Spatial<2> for SamplePoint {
     fn position(self: &Self) -> [f32; 2] {
         self.position.into()
     }
+}
+
+impl Spatial<5> for ImagePoint {
+    fn position(self: &Self) -> [f32; 5] {
+        [
+            self.position.x,
+            self.position.y,
+            self.color.r * screen_width(),
+            self.color.g * screen_height(),
+            self.color.b * screen_width(),
+        ]
+    }
+}
+
+async fn generate_image_data() -> Vec<ImagePoint> {
+    let tex = load_texture("image.png").await.unwrap();
+    let img = tex.get_texture_data();
+
+    let mut points = Vec::new();
+    for y in 0..tex.height() as u32 {
+        for x in 0..tex.width() as u32 {
+            points.push(ImagePoint {
+                position: vec2(x as f32, y as f32),
+                color: img.get_pixel(x, y),
+            })
+        }
+    }
+
+    return points;
 }
 
 fn generate_test_data() -> Vec<SamplePoint> {
@@ -67,9 +119,11 @@ async fn main() {
             .as_secs(),
     );
 
-    let sample_points = generate_test_data();
+    let sample_points = generate_image_data().await;
 
-    let points = scan(&sample_points, 5, 12.0);
+    let time = Instant::now();
+    let points = scan(&sample_points, 10, 15.0);
+    println!("{}s", time.elapsed().as_secs_f64());
 
     let mut colors: HashMap<_, Color> = HashMap::new();
     for i in 0..points.len() {
@@ -88,7 +142,14 @@ async fn main() {
                 .map(|i| colors.get(&i).unwrap().clone())
                 .unwrap_or(BLACK);
 
-            draw_circle(sample_point.position.x, sample_point.position.y, 5.0, color);
+            //draw_circle(sample_point.position.x, sample_point.position.y, 5.0, color);
+            draw_rectangle(
+                sample_point.position.x,
+                sample_point.position.y,
+                1.0,
+                1.0,
+                color,
+            );
         }
 
         next_frame().await
